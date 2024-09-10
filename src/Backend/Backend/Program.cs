1f57b281363d319eada98f8c0e8e11cdeea76d00
Cássio Morais <cassiomoraisg@hotmail.com>
2024-09-10 11:33:25 -0300

    feat(Docker) Add docker-compose and some other things

--- src/Backend/Backend/Program.cs
+++ src/Backend/Backend/Program.cs
@@ -1,11 +1,18 @@
 using Backend.Api.Core.Services;
 using Backend.Api.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt => 
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    opt.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
--- src/Frontend/frontend/src/environment.ts
+++ src/Frontend/frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://backend-api:8080/api', // base url docker
+    baseUrl: 'http://localhost:8081/api', // base url docker
     // locally without docker you can change the baseurl to 'http://localhost:5139/api'
   };
